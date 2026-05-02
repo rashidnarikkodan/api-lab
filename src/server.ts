de827662f5344596c4966ca774acda43b5393b83
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2026-05-02 12:51:17 +0530

    add health check endpoint to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from 'express'
 
 const app = express()
 
+app.get('/health',(req,res)=>{
+    res.json({status:'ok',message:"server is running"})
+})
 
 app.listen(3000,()=>{
     console.log('server running')
